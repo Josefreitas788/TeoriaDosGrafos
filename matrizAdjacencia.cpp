cf35685f72941e9b64511afff8ea083171450c25
Raul Bruno <47263040+RaulLima2@users.noreply.github.com>
2022-11-16 20:50:59 -0300

    Update matrizAdjacent in methods of data matrix

--- matrizAdjacencia.cpp
+++ matrizAdjacencia.cpp
@@ -1,37 +1,66 @@
 /*Faça em C/C++ um programa que construa uma matriz de adjacência a partir de informações fornecidas pelo usuário (qtde. de vértices, qtde. de arestas, arestas, ou seja, os vértices que definem cada aresta). Considerem os vértices como números.*/
 #include <iostream>
 using namespace std;
+
+void printMatrixAdjacent(int** matrix, int numeroVertices);
+int** insertMatrixAdjacent(int** matrix, int numeroVertices, int numeroArestas);
+int** inicializeMatrixAdjacent(int** matrix, int numeroVertices);
+
 int main()
 {
+    int i = 0;
+    int j = 0;
+    int numeroVertices = 0;
+    int numeroArestas = 0;
+    
     int n, m, i, j, k, l;
     cout << "Digite o numero de vertices: ";
-    cin >> n;
+    cin >> numeroVertices;
     cout << "Digite o numero de arestas: ";
-    cin >> m;
-    int matriz[n][n];
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
+    cin >> numeroArestas;
+    
+    int matriz[numeroVertices][numeroVertices];
+    
+    inicializeMatrixAdjacent(matriz, numeroVertices);
+    insertMatrixAdjacent(matriz, numeroVertices, numeroArestas);
+    printMatrixAdjacent(matriz, numeroVertices);
+    return 0;
+}
+
+int** inicializeMatrixAdjacent(int** matrix, int numeroVertices) {
+    for (i = 0; i < numeroVertices; i++){
+        for (j = 0; j < numeroVertices; j++)
         {
             matriz[i][j] = 0;
         }
     }
-    for (k = 0; k < m; k++)
+    
+    return matrix;
+}
+
+int** insertMatrixAdjacent(int** matrix, int numeroVertices, int numeroArestas) {
+    int i = 0;
+    int verticeOrigem = 0;
+    int verticeDestino = 0;
+    for (i = 0; i < numeroArestas; i++)
     {
         cout << "Digite o vertice de origem: ";
-        cin >> i;
+        cin >> verticeOrigem;
         cout << "Digite o vertice de destino: ";
-        cin >> j;
-        matriz[i][j] = 1;
-        matriz[j][i] = 1;
+        cin >> verticeDestino;
+        matriz[verticeOrigem][verticeDestino] = 1;
+        matriz[verticeDestino][verticeOrigem] = 1;
     }
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
+    
+    return matrix;
+}
+
+void printMatrixAdjacent(int** matrix, int numeroVertices) {
+    for (i = 0; i < numeroVertices; i++) {
+        for (j = 0; j < numeroVertices; j++)
         {
             cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
-    return 0;
 }
