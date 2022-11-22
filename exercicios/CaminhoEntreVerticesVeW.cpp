6a3dcaf2097656d548b21e2cab79bcd352df6418
Josefreitas788 <josefreitas788@gmail.com>
2022-11-21 23:09:25 -0300

    Organizando o repositório

---
+++ exercicios/CaminhoEntreVerticesVeW.cpp
@@ -0,0 +1,10 @@
+/*Um vértice v é atingível a partir de um vértice w em um grafo G, se v for igual a w ou se existir um caminho de w até v. Escreva uma função em C/C++ que receba como parâmetros uma matriz de adjacência e dois inteiros (vértices v e w) e determine se existe caminho ligando w até v.
+*/
+include<iostream>
+
+int main ()
+{
+    
+
+  return 0;
+}
