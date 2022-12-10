fe7cc60124b799a8388e1673b2ca9154b5735c71
Josefreitas788 <josefreitas788@gmail.com>
2022-12-10 15:34:48 -0300

    Modificando matriz de adjacencia

--- trabalho_1/codigos/grafo.py
+++ trabalho_1/codigos/grafo.py
@@ -1,8 +1,7 @@
-
+import numpy as np
 class Grafo:
     def __init__(self, input_file):
-        self.vertices = vertices
-        self.grafo = [[0 for i in range(vertices)] for j in range(vertices)]
+        self.read_file(input_file)
 
     def inserir_aresta(self, u, v):
         self.grafo[u][v] = 1
@@ -11,7 +10,7 @@ class Grafo:
         self.grafo[u][v] = 0
 
     def imprimir(self):
-        for i in range(self.vertices):
+        for i in range((self.vertices//2)+1):
             for j in range(self.vertices):
                 print(self.grafo[i][j], end=" ")
             print()
@@ -20,13 +19,15 @@ class Grafo:
         input = open(input_file, "r")
         n = 0
         for line in input:
-            print(line)
             x = line.split(" ")
             try:
                 x[1] = x[1].replace("\n", "")
-                this.inserir_aresta(int(x[0]), int(x[1]))
+                self.inserir_aresta(int(x[0]), int(x[1]))
             except:
                 self.vertices = int(x[0])
+                self.grafo = [[0 for i in range((self.vertices//2)+1)] for j in range(self.vertices)]
                 pass
 
-        
+
+g = Grafo("../as_graph.txt")
+g.imprimir()
