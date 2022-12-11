18ed0c3ba0299141f922fde3dfcfe0e3ed568976
HeloiseKatharine <katharineheloise@gmail.com>
2022-12-11 15:17:50 -0300

    Update

--- trabalho_1/codigos/matriz-de-adjacencia.py
+++ trabalho_1/codigos/matriz-de-adjacencia.py
@@ -3,29 +3,15 @@
 class GrafoAdjacente:
     def __init__(self, input_file):
         self.read_file(input_file)
-        # self.imprimir()
-      
+        self.imprimir()
 
     def imprimir(self):
-        print(self.vertices)
-  
-        # for i in range((self.vertices//2)+1):
-        #     for j in range(self.vertices):
-        #         print(self.grafo[i][j], end=" ")
-        #     print()
+        print('Matriz de adjacência = ', self.grafomatriz)
 
-    
-    # def matriz(self, i, j):
-    def matriz(self, lista):
-        print('-------------TESTE-------------')
-        print('lista[0]' , int(lista[0]))
-
+    # Adiciona 0 ou 1 na matriz
+    def preenche_matriz(self, lista):
         self.grafomatriz[int(lista[0]) -1 ][int(lista[1])-1 ] = 1
         self.grafomatriz[int(lista[1]) -1 ][int(lista[0])-1 ] = 1
-        
-        #print('self.vertices matriz = ', type(int(self.vertices[0])))
-        # print('lista matriz = ', lista)
-        # print('isso é a matriz = ', self.grafomatriz)
 
     def inserir_aresta(self, u, v):
         self.grafo[u][v] = 1
@@ -37,31 +23,19 @@ class GrafoAdjacente:
             line = line.replace("\n", "")
             x = line.split(" ")
             lista.append(x)
-
-        #######################################################
         
         self.vertices = lista[0]
         del lista[0]
-
 
         # Matriz 
         self.grafomatriz = []
         # linhas
         for i in range(0, int(self.vertices[0])):
-            print('i =', i)
             self.grafomatriz.append([])
             for j in range (0, int(self.vertices[0])):
-            # self.grafomatriz[i][j] = 0
                 self.grafomatriz[i].append(0)
-        print('isso é a matriz = ', self.grafomatriz)
 
-        #passandoa a lista para a matriz 
         for x in lista:
-            self.matriz(x)
-        print('isso é a matriz = ', self.grafomatriz)
-
-    
+            self.preenche_matriz(x)
 
 g = GrafoAdjacente("../teste.txt")
-
-
